3b290ad06a6ffd5543d468fa8c101610a6b8d7e4
Мария Лисицкая <lisitskayamari@gmail.com>
2021-04-12 18:33:33 +0100

    all urls files correct

--- posts/urls.py
+++ posts/urls.py
@@ -3,14 +3,34 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("new/", views.new_post, name="new_post"),
-    path("group/<slug:slug>/", views.group_posts, name="group_posts"),
-    path('<str:username>/', views.profile, name='profile'),
-    path('<str:username>/<int:post_id>/', views.post_view, name='post'),
+    path(
+        'new/',
+        views.new_post,
+        name='new_post'
+    ),
+    path(
+        'group/<slug:slug>/',
+        views.group_posts,
+        name='group_posts'
+    ),
+    path(
+        '<str:username>/',
+        views.profile,
+        name='profile'
+    ),
+    path(
+        '<str:username>/<int:post_id>/',
+        views.post_view,
+        name='post'
+    ),
     path(
         '<str:username>/<int:post_id>/edit/',
         views.post_edit,
         name='post_edit'
     ),
-    path("", views.index, name="index"),
+    path(
+        '',
+        views.index,
+        name='index'
+    ),
 ]
